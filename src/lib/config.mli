3dc216e9e8c154df76705a1cb4b30d74aaeb6a2b
TheLusitanianKing <alex.lisboa.metalhead@gmail.com>
2023-10-24 17:46:26 +0100

    indentation

--- src/lib/config.mli
+++ src/lib/config.mli
@@ -4,8 +4,12 @@ val get_config_map_from_map_of_alist
   : [< `Duplicate_key of 'a | `Ok of (string, 'b, Base.String.comparator_witness) Base.Map.t]
   -> (string, 'b, Base.String.comparator_witness) Base.Map.t
 
-val read_config_file : string -> (string, string, Base.String.comparator_witness) Base.Map.t
+val read_config_file : string
+  -> (string, string, Base.String.comparator_witness) Base.Map.t
 
-val read_config_param_from_config_map : ('a, 'b, 'c) Base.Map.t -> key:'a -> 'b option
+val read_config_param_from_config_map
+  : ('a, 'b, 'c) Base.Map.t 
+  -> key:'a
+  -> 'b option
 
 val read_config_param : string -> string -> string option
