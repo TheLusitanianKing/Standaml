45db14489b26aab5beb006bda91993f8defd5fb8
TheLusitanianKing <alex.lisboa.metalhead@gmail.com>
2023-10-27 17:11:34 +0100

    a bit of indent and verbose stuff

--- src/bin/main.ml
+++ src/bin/main.ml
@@ -1,6 +1,10 @@
 let () =
-  let config_map = Standaml.Config.read_config_file "standaml.conf" in
-  let opt_token = Standaml.Config.read_config_param_from_config_map config_map ~key:"" in
+  let config_map = Standaml.Config.read_config_file "../standaml.conf" in
+  let opt_token =
+    Standaml.Config.read_config_param_from_config_map
+      config_map ~key:"FOOTBALL_API_TOKEN" in
   match opt_token with
-  | None -> print_endline "You need to specify your token in the config file `standaml.conf`."
-  | Some _token -> print_endline "Hello football fans"
+    | None ->
+      print_endline "No token specified in `standaml.conf`."
+    | Some token ->
+      print_endline @@ Printf.sprintf "Hello football fans (token is: %s)" token
