4247318fa142c0e6e1a9c13d28d581086b8d67ee
TheLusitanianKing <alex.lisboa.metalhead@gmail.com>
2023-10-28 16:49:26 +0100

    SSL + HTTP request dependencies and example

--- src/bin/main.ml
+++ src/bin/main.ml
@@ -1,3 +1,16 @@
+open Lwt
+open Cohttp
+open Cohttp_lwt_unix
+
+let body =
+  Client.get (Uri.of_string "https://reidelusitania.pt/") >>= fun (resp, body) ->
+  let code = resp |> Response.status |> Code.code_of_status in
+  Printf.printf "Response code: %d\n" code;
+  Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string);
+  body |> Cohttp_lwt.Body.to_string >|= fun body ->
+  Printf.printf "Body of length: %d\n" (String.length body);
+  body
+
 let () =
   let config_map = Standaml.Config.read_config_file "../standaml.conf" in
   let opt_token =
@@ -7,4 +20,6 @@ let () =
     | None ->
       print_endline "No token specified in `standaml.conf`."
     | Some token ->
-      print_endline @@ Printf.sprintf "Hello football fans (token is: %s)" token
+      let resp = Lwt_main.run body in
+      print_endline @@
+        Printf.sprintf "Hello football fans (token is: %s)\n%s" token resp
