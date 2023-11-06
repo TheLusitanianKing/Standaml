50fbb79ec4f7c6aaa001f6a6869604d75702efc4
TheLusitanianKing <alex.lisboa.metalhead@gmail.com>
2023-11-06 16:57:18 +0000

    sorting (although it's already sorted in the API, better to be sure)

--- src/lib/tui/standing.ml
+++ src/lib/tui/standing.ml
@@ -7,9 +7,12 @@ let display_standing_line ~(standing_line:Model.Standing_line.t) =
 
 let display_standing ~(standing:Model.Standing.t) ~standing_format =
   let standing_lines = standing.team_standings in
-  let formatted_lines = standing_lines
+  let sorted_lines = standing_lines
+    |> List.sort ~compare:Model.Standing_line.compare
+    |> List.rev in
+  let formatted_lines = sorted_lines
     |> List.map ~f:(fun standing_line -> display_standing_line ~standing_line) in
   let open Standing_format in
   match standing_format with
   | Default  -> formatted_lines |> String.concat ~sep:"\n" 
-  | One_line -> formatted_lines |> String.concat ~sep:"-"
+  | One_line -> formatted_lines |> String.concat ~sep:" | "
