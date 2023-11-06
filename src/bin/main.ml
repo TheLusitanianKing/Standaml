3dc8ec09b6ccfa8de8fe4d8048a696a3ca84860c
TheLusitanianKing <alex.lisboa.metalhead@gmail.com>
2023-11-06 17:28:48 +0000

    option parsing start

--- src/bin/main.ml
+++ src/bin/main.ml
@@ -1,16 +1,30 @@
 open Base
 
-let () =
+(* let usage_msg = "standaml -n <HOW_MANY_TEAMS_PER_COMP> -t <TOKEN> -f <FORMAT> <COMPETITION_CODE_1> [<COMPETITION_CODE_2>]" *)
+
+let format = ref Standaml.Tui.Standing_format.Default
+let arg_token = ref None
+
+let get_token_from_config_or_fail =
   let config_map = Standaml.Config.read_config_file "../standaml.conf" in
   let opt_token =
     Standaml.Config.read_config_param_from_config_map
       config_map ~key:"FOOTBALL_API_TOKEN" in
   match opt_token with
-  | None -> failwith "No token specified in `standaml.conf`."
-  | Some token ->
-    let opt_standing = Lwt_main.run @@ Standaml.Api.fetch_standing ~token ~competition:"PPL" in
-    match opt_standing with
-    | None -> failwith "Standing couldn't be fetched"
-    | Some standing ->
-      let standing_display = Standaml.Tui.Standing.display_standing ~standing ~standing_format:Standaml.Tui.Standing_format.Default in
-      Stdio.print_endline standing_display
+  | None -> failwith "No token specified in `standaml.conf` or ...."
+  | Some conf_token -> conf_token
+
+let get_token_or_fail token =
+  match !token with
+  | None -> get_token_from_config_or_fail
+  | Some t -> t
+
+let () =
+  let token = get_token_or_fail arg_token in
+  let competition = "PL" in
+  let opt_standing = Lwt_main.run @@ Standaml.Api.fetch_standing ~token ~competition in
+  match opt_standing with
+  | None -> Stdio.print_endline @@ Printf.sprintf "Couldn't fetch standing for competition %s" competition
+  | Some standing ->
+    let standing_display = Standaml.Tui.Standing.display_standing ~standing ~standing_format:!format in
+    Stdio.print_endline standing_display
