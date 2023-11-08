0b036a5494420639692231ba14a78fd3af46428b
Alexandre Gomes <alex.lisboa.metalhead@gmail.com>
2023-11-08 19:55:52 +0000

    retrieve position from the API and sort accordingly (fixes #8)

--- src/lib/model/standing_line.ml
+++ src/lib/model/standing_line.ml
@@ -1,5 +1,6 @@
 type t =
   { team_name: string
+  ; position: int
   ; played_games: int
   ; won: int
   ; draw: int
@@ -12,13 +13,14 @@ let parse ~json =
   let open Yojson.Basic.Util in
   try
     let team_name = json |> member "team" |> member "name" |> to_string in
+    let position = json |> member "position" |> to_int in
     let played_games = json |> member "playedGames" |> to_int in
     let won = json |> member "won" |> to_int in
     let lost = json |> member "lost" |> to_int in
     let draw = json |> member "draw" |> to_int in
     let goals_for = json |> member "goalsFor" |> to_int in
     let goals_against = json |> member "goalsAgainst" |> to_int in
-    Some { team_name; played_games; won; draw; lost; goals_for; goals_against }
+    Some { team_name; position; played_games; won; draw; lost; goals_for; goals_against }
   with
     Type_error _ -> None
 
@@ -28,33 +30,4 @@ let points x =
 let goals_difference x =
   x.goals_for - x.goals_against
 
-(* as a reminder,
-   the comparison function must return 0 if its arguments compare as equal,
-   a positive integer if the first is greater,
-   and a negative integer if the first is smaller *)
-let compare (x:t) (y:t): int =
-  let points_diff = points x - points y in
-  let goals_diff = goals_difference x - goals_difference y in
-  if points_diff = 0 (* points first *)
-    then goals_diff (* goals difference second *)
-    else points_diff
-
-let%test "compare (total equality)" =
-  let x = { team_name = "A"; played_games = 4; won = 2; draw = 1; lost = 1; goals_for = 5; goals_against = 3 } in
-  let y = { team_name = "B"; played_games = 4; won = 2; draw = 1; lost = 1; goals_for = 5; goals_against = 3 } in
-  compare x y = 0
-
-let%test "compare (equality)" =
-  let x = { team_name = "A"; played_games = 4; won = 1; draw = 0; lost = 2; goals_for = 3; goals_against = 3 } in
-  let y = { team_name = "B"; played_games = 4; won = 0; draw = 3; lost = 0; goals_for = 3; goals_against = 3 } in
-  compare x y = 0
-
-let%test "compare (more points)" =
-  let x = { team_name = "A"; played_games = 1; won = 1; draw = 0; lost = 0; goals_for = 3; goals_against = 0 } in
-  let y = { team_name = "B"; played_games = 1; won = 0; draw = 0; lost = 1; goals_for = 3; goals_against = 4 } in
-  compare x y > 0
-
-let%test "compare (same points, best goals difference)" =
-  let x = { team_name = "A"; played_games = 1; won = 1; draw = 0; lost = 0; goals_for = 3; goals_against = 0 } in
-  let y = { team_name = "B"; played_games = 1; won = 1; draw = 0; lost = 0; goals_for = 1; goals_against = 0 } in
-  compare x y > 0
+let compare (x:t) (y:t): int = x.position - y.position
--- src/lib/tui/standing.ml
+++ src/lib/tui/standing.ml
@@ -1,11 +1,11 @@
 open Base
 
-let display_standing_line ~(standing_line:Model.Standing_line.t) ~standing_format ~index =
+let display_standing_line ~(standing_line:Model.Standing_line.t) ~standing_format =
   let open Standing_format in
   match standing_format with
   | Simple ->
       Printf.sprintf "%d. P%d - %s (%d pts) (%d-%d)"
-        (index + 1)
+        standing_line.position
         standing_line.played_games
         standing_line.team_name
         (Model.Standing_line.points standing_line)
@@ -13,19 +13,17 @@ let display_standing_line ~(standing_line:Model.Standing_line.t) ~standing_forma
         standing_line.goals_against
   | One_line ->
       Printf.sprintf "%d. %s (%d pts)"
-        (index + 1)
+        standing_line.position
         standing_line.team_name
         (Model.Standing_line.points standing_line)
 
 let display_standing ~(standing:Model.Standing.t) ~standing_format ~limit =
-  let standing_lines = standing.team_standings in
-  let sorted_lines = standing_lines
-    |> List.sort ~compare:Model.Standing_line.compare
-    |> List.rev in
-  let opt_trimming = Option.bind limit ~f:(fun l -> Option.return @@ List.take sorted_lines l) in
-  let trimmed_lines = Option.value opt_trimming ~default:sorted_lines in
+  let sorted_standing_lines = standing.team_standings
+    |> List.sort ~compare:Model.Standing_line.compare in
+  let opt_trimming = Option.bind limit ~f:(fun l -> Option.return @@ List.take sorted_standing_lines l) in
+  let trimmed_lines = Option.value opt_trimming ~default:sorted_standing_lines in
   let formatted_lines = trimmed_lines
-    |> List.mapi ~f:(fun index standing_line -> display_standing_line ~standing_line ~index ~standing_format) in
+    |> List.map ~f:(fun standing_line -> display_standing_line ~standing_line ~standing_format) in
   let open Standing_format in
   match standing_format with
   | Simple   -> formatted_lines |> String.concat ~sep:"\n" 
