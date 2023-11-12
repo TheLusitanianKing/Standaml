1e26f604c227e3f46d0dfe426658616f650a0eb6
Alexandre Gomes <alex.lisboa.metalhead@gmail.com>
2023-11-12 15:04:53 +0000

    fill function for strings

--- src/lib/tui/matrix.ml
+++ src/lib/tui/matrix.ml
@@ -1,5 +1,24 @@
 open Base
 
+let fill content n =
+  let rec helper acc s i =
+    if i >= n
+      then acc
+      else
+        match s with
+        | [] -> acc @ List.init (n-i) ~f:(fun _ -> ' ')
+        | x::xs -> helper (acc @ [x]) xs (i+1)
+  in String.of_char_list @@ helper [] (String.to_list content) 0
+
+let%test "fill (1)" =
+  String.equal (fill "AB" 3) "AB "
+
+let%test "fill (2)" =
+  String.equal (fill "AB" 2) "AB"
+
+let%test "fill (3)" =
+  String.equal (fill "AB" 0) ""
+
 (** list of list of string (+ the max size, for each column) *)
 let pretty_display (matrix: (int * string list) list) =
   String.concat ~sep:"\n" @@ List.map ~f:(fun _col -> failwith "TODO") matrix 
